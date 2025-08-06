d0c097c9955d63533751b55f1cc3bfedfa086d0a
maxzadi <maxzadi20@gmail.com>
2025-08-05 20:33:23 -0600

    Inicio controlador

--- Main.java
+++ Main.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner leer = new Scanner(System.in);
-        System.out.println("null");
+        PokemonControlador controlador = new PokemonControlador();
+
+        System.out.println("============== ¡Empieza el combate! ==============");
+        while(true){
+        System.out.println("Entrenador 1 debe escoger un pokemon: ");
+        
+        
+        }
     }
 }
--- PokemonControlador.java
+++ PokemonControlador.java
@@ -1,5 +1,35 @@
+import modelos.Entrenador;
+import modelos.Pokemon;
+import modelos.HabilidadEspecial;
+import modelos.Ronda;
 package laboratorio1poo;
 
 public class PokemonControlador {
-    
+    public Entrenador entrenador1;
+    public Entrenador entrenador2;
+
+
+       pokemones.add(new Pokemon("Charizard", "Fuego", 84, 78,
+            new HabilidadEspecial("Llamarada", "Quemadura", 30, 60)));
+
+        pokemones.add(new Pokemon("Blastoise", "Agua", 83, 100,
+            new HabilidadEspecial("Hidrobomba", "Reducción de ataque", 25, 50)));
+
+        pokemones.add(new Pokemon("Venusaur", "Planta", 82, 83,
+            new HabilidadEspecial("Latigazo", "Parálisis", 20, 40)));
+
+        pokemones.add(new Pokemon("Pikachu", "Eléctrico", 55, 40,
+            new HabilidadEspecial("Impactrueno", "Parálisis", 15, 70)));
+
+        pokemones.add(new Pokemon("Gengar", "Fantasma", 65, 60,
+            new HabilidadEspecial("Hipnosis", "Dormir", 0, 60)));
+
+        pokemones.add(new Pokemon("Alakazam", "Psíquico", 90, 45,
+            new HabilidadEspecial("Psíquico", "Reducción de defensa", 30, 50)));
+
+        pokemones.add(new Pokemon("Machamp", "Lucha", 130, 80,
+            new HabilidadEspecial("Puño Dinámico", "Confusión", 25, 50)));
+
+        pokemones.add(new Pokemon("Snorlax", "Normal", 110, 65,
+            new HabilidadEspecial("Descanso", "Curación", 50, 100)));
 }
